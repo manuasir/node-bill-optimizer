444649204de4c9d998557490d0e1a23112a89957
manuasir <manuel.jimenez@wazuh.com>
2020-10-22 03:10:07 +0200

    Added default param as class attribute

--- src/domain/redata-api/redata-api.js
+++ src/domain/redata-api/redata-api.js
@@ -8,12 +8,13 @@ class ApiWrapper {
     this.baseUrl = 'https://api.esios.ree.es'
     this.token = token
     this.request = request
+    this.defaultResource = '/indicators/1014'
   }
 
   async get (options) {
     try {
       const headers = { headers: { Authorization: `Token token=${this.token}` } }
-      let url = ((options && options.resource) ? this.baseUrl + options.resource : this.baseUrl + '/indicators/1014')
+      let url = ((options && options.resource) ? this.baseUrl + options.resource : this.baseUrl + this.defaultResource)
       url = (options && options.params && options.params.end_date && options.params.start_date) ? url + `?start_date=${options.params.start_date}&end_date=${options.params.end_date}` : url + ''
       const result = await this.request.get(url, headers)
       return result.body.indicator.values
