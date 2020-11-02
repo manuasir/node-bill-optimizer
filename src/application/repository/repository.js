1139e774608555511e98d06daaf55af0f343c39c
manuasir <manuel.jimenez@wazuh.com>
2020-11-02 04:02:27 +0100

    Fixed cache

--- src/application/repository/repository.js
+++ src/application/repository/repository.js
@@ -19,10 +19,7 @@ class DataRepository {
    */
   async updateCache(start, end) {
     try {
-
-
       const result = await this.pvpc.getPriceInRangeDate(start, end)
-
       const tempArr = [...this.cache, ...result]
       this.cache = tempArr.filter((thing, index) => {
         const _thing = JSON.stringify(thing.datetime)
@@ -46,24 +43,15 @@ class DataRepository {
     return data
   }
 
-
   filterData(start, end) {
-
-
-
     const result = this.cache.filter((item) => {
-      const date = new Date(item.datetime)
+      const date = new Date(item.datetime.split('.')[0])
+      date.setTime( date.getTime() - new Date().getTimezoneOffset()*60*1000 )
       const startDate = new Date(start)
       startDate.setTime( startDate.getTime() - new Date().getTimezoneOffset()*60*1000 )
       const endDate = new Date(end)
       endDate.setTime( endDate.getTime() - new Date().getTimezoneOffset()*60*1000 )
-     // if(date.getTime() < startDate.getTime()) {
-
-    //  }
-      //if(date.getTime() > endDate.getTime()) {
-
-     // }
-      return date.getTime() >= startDate.getTime() && date.getTime() < endDate.getTime()
+      return date.getTime() >= startDate.getTime() && date.getTime() <= endDate.getTime()
     })
 
     return result
@@ -78,38 +66,29 @@ class DataRepository {
   async get(start, end) {
     try {
       const now = new Date().addDays(1)
-
       const date = new Date(start)
       date.setTime( date.getTime() - new Date().getTimezoneOffset()*60*1000 )
-
-
       if (date > now || this.checkHoursDiff(start,end) > 25) {
-
-
-
         throw new Error('Cannot fetch data with this time range.')
       }
       // If cache is empty, reload it
       if (this.cache.length === 0) {
-
         await this.updateCache(start, end)
       }
       let data = this.filterData(start, end)
 
       // If there is data in cache but not the results, fetch and cache them
-      if (!data || !Array.isArray(data) || data.length === 0 ) {
-
+      if (!data || !Array.isArray(data) || data.length === 0 || data.length < this.checkHoursDiff(start,end)) {
         await this.updateCache(start, end)
         data = this.filterData(start, end)
       }
+
       const enrichData = this.addConsultedTimes(data)
       return enrichData
     } catch (error) {
       return Promise.reject(error)
     }
   }
-
-
 }
 
 module.exports = DataRepository
--- token.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "token": "",
-    "tg_token": ""
-}
